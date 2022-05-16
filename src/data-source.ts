cb05911b8d294c269aa07ed777a6360d27098f95
Anderson Pinto <74440689+andersonstpinto@users.noreply.github.com>
2022-05-16 14:33:28 -0300

    fix: updated missing test dependencies

--- src/data-source.ts
+++ src/data-source.ts
@@ -3,18 +3,26 @@ import { DataSource } from "typeorm";
 
 require("dotenv").config();
 
-export const AppDataSource = new DataSource({
-  type: "postgres",
-  host: "localhost",
-  port: 5432,
+export const AppDataSource =
+  process.env.NODE_ENV === "test"
+    ? new DataSource({
+        type: "sqlite",
+        database: ":memory:",
+        synchronize: true,
+        entities: ["src/entities/*.ts"],
+      })
+    : new DataSource({
+        type: "postgres",
+        host: "localhost",
+        port: 5432,
 
-  username: process.env.POSTGRES_USER,
-  password: process.env.POSTGRES_PWD,
-  database: process.env.POSTGRES_DB,
+        username: process.env.POSTGRES_USER,
+        password: process.env.POSTGRES_PWD,
+        database: process.env.POSTGRES_DB,
 
-  synchronize: false,
-  logging: true,
+        synchronize: false,
+        logging: true,
 
-  entities: ["src/entities/*.ts"],
-  migrations: ["src/migrations/*.ts"],
-});
+        entities: ["src/entities/*.ts"],
+        migrations: ["src/migrations/*.ts"],
+      });
