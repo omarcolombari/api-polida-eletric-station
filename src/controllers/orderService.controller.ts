55dcecfb47fd6853d31db4488be22fd061f05217
Omar <omarucolombari@gmail.com>
2022-05-19 10:06:33 -0300

    Fix: colocado awawait nos controller

--- src/controllers/orderService.controller.ts
+++ src/controllers/orderService.controller.ts
@@ -14,26 +14,26 @@ export default class OrderServiceController {
   }
   static async index(req: Request, res: Response) {
     const listOrders = new listOrderService();
-    const orders = listOrders.execute();
+    const orders = await listOrders.execute();
     return res.json(orders);
   }
   static async show(req: Request, res: Response) {
     const { id } = req.params;
     const showOrder = new ShowOrderService();
-    const order = showOrder.execute(id);
+    const order = await showOrder.execute(id);
     return res.json(order);
   }
   static async update(req: Request, res: Response) {
     const { id } = req.params;
     const { status, reschedule } = req.body;
     const updateOrder = new UpdateOrderService();
-    const updatedOrder = updateOrder.execute({ id, reschedule, status });
+    const updatedOrder = await updateOrder.execute({ id, reschedule, status });
     return res.status(201).json(updatedOrder);
   }
 
   static async showPerUserId(req: Request, res: Response) {
-    /*const { id } = req.user;
+    const { id } = req.user;
     const showOrderPerIdService = new ShowOrderPerIdService();
-    const orders = showOrderPerIdService.execute(id);*/
+    const orders = await showOrderPerIdService.execute(id);
   }
 }
