11f62f8cab6aa10da76c012ce9394b5e146d012c
Omar <omarucolombari@gmail.com>
2022-05-19 14:33:52 -0300

    Fix: arrumado problema no tsconfig.json

--- src/data-source.ts
+++ src/data-source.ts
@@ -14,7 +14,7 @@ export const AppDataSource =
     : new DataSource({
         type: "postgres",
         host: "db",
-        port: 45432,
+        port: 5432,
         username: process.env.POSTGRES_USER,
         password: process.env.POSTGRES_PASSWORD,
         database: process.env.POSTGRES_DB,
