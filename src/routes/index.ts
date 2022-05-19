f45004f626a36ebc1bcc1068e5dc48dc2aa9521c
Paulo Sousa <psluna.sousa@gmail.com>
2022-05-19 11:00:49 -0300

    fix: Restructure units routes path

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,6 @@
 import { Express } from "express";
 import clientRouter from "./clients/clients.routes";
-import { unitRoutes } from "./units.routes";
+import { unitRoutes } from "../routes/units/units.routes";
 
 export default (app: Express) => {
   app.use("/clients", clientRouter);
--- src/routes/units/units.routes.ts
+++ src/routes/units/units.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { UnitsController } from "../controllers/units.controller";
+import { UnitsController } from "../../controllers/units.controller";
 
 const router = Router();
 
