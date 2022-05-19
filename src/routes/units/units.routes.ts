c9dfa1c4ad3d37c3d2325a2270aeee1badefddaa
Anderson Pinto <74440689+andersonstpinto@users.noreply.github.com>
2022-05-19 20:21:03 -0300

    feat: added admin and auth middlewares

--- src/routes/units/units.routes.ts
+++ src/routes/units/units.routes.ts
@@ -1,9 +1,14 @@
 import { Router } from "express";
 import { UnitsController } from "../../controllers/units.controller";
+import ensureAuthMiddleware from "../../middlewares/ensureAuth.middleware";
+import verifyAdminMiddleware from "../../middlewares/verifyAdmin.middleware";
 
 const router = Router();
 
 export const unitRoutes = () => {
+  router.use(ensureAuthMiddleware);
+  router.use(verifyAdminMiddleware);
+
   router.post("/", UnitsController.store);
   router.patch("/:unit_id", UnitsController.update);
   router.delete("/:unit_id", UnitsController.delete);
