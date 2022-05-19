399d7681c93308459cbe09bd6b5b5c7917a2da85
Paulo Sousa <psluna.sousa@gmail.com>
2022-05-19 16:49:03 -0300

    fix: Fixed import errors from tests

--- src/routes/orderServiceRoutes/orderServices.routes.ts
+++ src/routes/orderServiceRoutes/orderServices.routes.ts
@@ -5,7 +5,7 @@ const orderRouter = Router();
 
 export default () => {
   orderRouter.post("/", OrderServiceController.store);
-  orderRouter.get("/all", OrderServiceController.index);
+  orderRouter.get("/", OrderServiceController.index);
   orderRouter.get("/:id", OrderServiceController.show);
   orderRouter.get("/me", OrderServiceController.showPerUserId);
   orderRouter.patch("/:id", OrderServiceController.update);
