a8dd4d4df1af6697ca3c74731e2422db557967b4
Omar <omarucolombari@gmail.com>
2022-05-20 11:10:13 -0300

    Fix: ajustado o middleware para separar o Bearer do token

--- src/middlewares/ensureAuth.middleware.ts
+++ src/middlewares/ensureAuth.middleware.ts
@@ -8,7 +8,7 @@ const ensureAuthMiddleware = (
   next: NextFunction
 ) => {
   try {
-    const token = req.headers.authorization;
+    const token = req.headers.authorization?.split(" ")[1];
 
     if (!token) {
       throw new AppError("Token is missing.", 401);
