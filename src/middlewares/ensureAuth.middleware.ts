3a60aeda97a4f946c7d59de9d904acca84064856
Luiz Antonio Motta <mottaluizantonio@gmail.com>
2022-05-20 13:20:25 -0300

    fix: ensureAuth and verifyAdmin middlewares no longer are 'try/catch' structures.

--- src/middlewares/ensureAuth.middleware.ts
+++ src/middlewares/ensureAuth.middleware.ts
@@ -7,33 +7,27 @@ const ensureAuthMiddleware = (
   res: Response,
   next: NextFunction
 ) => {
-  try {
-    const token = req.headers.authorization;
+  const token = req.headers.authorization;
 
-    if (!token) {
-      throw new AppError("Token is missing.", 401);
-    }
-
-    verify(
-      token as string,
-      process.env.JWT_SECRET as string,
-      (err: any, decoded: any) => {
-        if (err) {
-          throw new AppError("Invalid Token", 401);
-        }
-
-        const { sub, isAdmin, name } = decoded;
-
-        req.user = { id: sub, name, isAdmin };
-
-        next();
-      }
-    );
-  } catch (err) {
-    if (err instanceof AppError) {
-      AppErrorHandler(err, res);
-    }
+  if (!token) {
+    throw new AppError("Token is missing.", 401);
   }
+
+  verify(
+    token as string,
+    process.env.JWT_SECRET as string,
+    (err: any, decoded: any) => {
+      if (err) {
+        throw new AppError("Invalid Token", 401);
+      }
+
+      const { sub, isAdmin, name } = decoded;
+
+      req.user = { id: sub, name, isAdmin };
+
+      next();
+    }
+  );
 };
 
 export default ensureAuthMiddleware;
--- src/middlewares/verifyAdmin.middleware.ts
+++ src/middlewares/verifyAdmin.middleware.ts
@@ -6,19 +6,13 @@ const verifyAdminMiddleware = (
   res: Response,
   next: NextFunction
 ) => {
-  try {
-    const { isAdmin } = req.user;
+  const { isAdmin } = req.user;
 
-    if (!isAdmin) {
-      throw new AppError("User is not admin.", 409);
-    }
-
-    next();
-  } catch (err) {
-    if (err instanceof AppError) {
-      AppErrorHandler(err, res);
-    }
+  if (!isAdmin) {
+    throw new AppError("User is not admin.", 409);
   }
+
+  next();
 };
 
 export default verifyAdminMiddleware;
