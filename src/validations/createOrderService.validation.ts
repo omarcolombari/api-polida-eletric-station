a12cd8b1dd49e0907aecebadbe6e456abf9fd3c3
Omar <omarucolombari@gmail.com>
2022-05-23 14:56:30 -0300

    Fix: colocado validação do unitId na criação do orderService

--- src/validations/createOrderService.validation.ts
+++ src/validations/createOrderService.validation.ts
@@ -1,11 +1,11 @@
 import * as yup from "yup";
 
-const wcreateOrderServiceSchema = {
+const createOrderServiceSchema = {
   schema: {
     body: {
       yupSchema: yup.object().shape({
         userId: yup.string().required("User id is required"),
-        clientId: yup.string().required("Client id is required"),
+        unitId: yup.string().required("Unit id is required"),
         serviceTypeId: yup.string().required("Service id is required"),
       }),
       validateOptions: {
